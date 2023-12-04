e0f7a40ff4d99fab087f1645d594be2f98fae8ab
filipjanevski27 <114435733+filipjanevski27@users.noreply.github.com>
2023-12-04 15:19:31 -0500

    updated Speech_to_text.py
    added file management system
    updated terminal responses to user for better understanding
    added vocal commands: create, open, delete, undo line

--- Speech_to_text.py
+++ Speech_to_text.py
@@ -1,8 +1,10 @@
+import os
 import speech_recognition as sr
 
 # Initializing the recognizer
 r = sr.Recognizer()
 
+file = "output.txt"
 
 def record_text():
     # while loop in case of errors
@@ -31,17 +33,83 @@ def record_text():
             print("unknown error occurred")
             return ""  # Return an empty string in case of an unknown value error
 
-
-def output_text(text):
-    f = open("output.txt", "a")
+def output_text(text, file):
+    f = open("./files/" + file, 'a')
     f.write(text)
     f.write("\n")
     f.close()
     return
 
+def undo_text(file):
+    f = open("./files/" + file, 'r+')
+    lines = f.readlines()
+    f.seek(0)
+    f.truncate()
+    f.writelines(lines[:-1])
+    f.close()
+    return
 
+print("Welcome to Voice-to-Code!\nSay \"help\" for a list of commands.\nDefault file = files/output.txt\n" +
+      "Begin speaking...")
+print(">", end="")
 while (1):
     text = record_text()
-    output_text(text)
-
-    print("Wrote text")
+    print(text)
+    if text == "new file": #Create file
+        print("Input name for new file: ")
+        file = (record_text() + ".txt")
+        f = open("./files/" + file, 'a')
+        f.close()
+        print("New file (" + file + ") created")
+        print(">", end="")
+    elif text == "open file": #Open existing file (or make new one if non-existant)
+        print("Input name of file: ")
+        file = (record_text() + ".txt")
+        if os.path.isfile("./files/" + file):
+            print("File " + file + " opened")
+            print(">", end="")
+        else:
+            print("File " + file + " does not exist. \nWould you like create this file? ('yes' to create)")
+            if record_text() == "yes" or "Yes":
+                f = open("./files/" + file, 'a')
+                f.close()
+                print("New file (" + file + ") created")
+                print(">", end="")
+            else:
+                print("No file created")
+                print(">", end="")
+    elif text == "delete file": #Delete file
+        print("Input name of file: ")
+        file = (record_text() + ".txt")
+        if os.path.isfile("./files/" + file):
+            print("Are you sure you would like to delete " + file + "? (Say 'yes' to confirm)")
+            if record_text() == "yes" or "Yes":
+                os.remove("./files/" + file)
+                print("File " + file + " deleted. Opened output.txt")
+                file = "output.txt"
+                print(">", end="")
+            else:
+                file = "output.txt"
+                print("No file deleted. Opened output.txt")
+                print(">", end="")
+        else:
+            file = "output.txt"
+            print("File " + file + " does not exist. Opened output.txt")
+            print(">", end="")
+    elif text == "undo text": #Delete last line written
+        undo_text(file)
+        print("Deleted last line of " + file)
+        print(">", end="")
+    elif text == "help":
+        print("Here are the list of vocal commands:\n'help' - Display this list of commands\n" +
+              "'new file' - create a txt file\n'open file' - open existing file\n" +
+              "'delete file' - delete text file\n'undo text' - delete last line of current opened file")
+        print(">", end="")
+    else: #Write text to current file
+        if text == "":
+            print("Wrote nothing")
+            print(">", end="")
+        else:
+            output_text(text, file)
+            print("Wrote text to " + file)
+            print(">", end="")
